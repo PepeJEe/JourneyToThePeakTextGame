646774478d91df38cf10387250dc959606a36b6c
PepeJEe <perttu.vierimaa@gmail.com>
2025-01-07 02:01:54 +0200

    Initial commit

---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [TextArea(10, 14)] [SerializeField] string storyText;
+
+    [SerializeField] State[] nextStates;
+
+
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+    public State[] GetNextStates()
+    {
+        return nextStates;
+    }
+
+}
